1b3313686c4e0dd69a5603e0a572390766557e6c
Aleksa Stancev <a.stancev@elfak.rs>
2021-12-08 23:56:52 +0100

    Initial file structure

---
+++ Implementacija/view.py
@@ -0,0 +1,27 @@
+def show_table():
+    return
+
+def show_end_screen():
+    #igraj opet
+    return
+
+def show_start_screen():
+    return
+
+def clear_console():
+    return
+
+def read_move():
+    return
+
+def read_first_player():
+    return
+
+def read_table_size():
+    return
+
+def read_wall_count():
+    return
+
+def read_start_positions():
+    return

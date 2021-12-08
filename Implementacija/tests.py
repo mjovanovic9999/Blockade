1a83b51f540451ad806e96e001cf75f1edf2ed94
Milos <m.jovanovic@elfak.rs>
2021-12-09 00:09:57 +0100

    c

--- Implementacija/tests.py
+++ Implementacija/tests.py
@@ -1,1 +1,3 @@
-#
+from view import *
+
+clear_console()
--- Implementacija/view.py
+++ Implementacija/view.py
@@ -1,27 +1,37 @@
 def show_table():
     return
 
+
 def show_end_screen():
-    #igraj opet
+    # igraj opet
     return
+
 
 def show_start_screen():
     return
 
+
 def clear_console():
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
     return
 
-def read_move():
+
+def read_move() -> list():
     return
+
 
 def read_first_player():
     return
 
+
 def read_table_size():
     return
 
+
 def read_wall_count():
     return
+
 
 def read_start_positions():
     return

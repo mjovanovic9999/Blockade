6a19fc2e9d65e3276fdcd3b200f904900fcdc3c6
Aleksa Stancev <a.stancev@elfak.rs>
2021-12-09 00:08:25 +0100

    Gitignore added

--- Implementacija/moves.py
+++ Implementacija/moves.py
@@ -1,14 +1,18 @@
 def is_game_end():
     return
 
+
 def is_player_movement_valid():
     return
+
 
 def is_wall_place_valid():
     return
 
+
 def move_player():
     return
+
 
 def place_wall():
     return

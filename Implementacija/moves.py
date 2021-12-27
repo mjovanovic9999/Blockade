03e05ae8a2143128d448e1413411041926791644
mjovanovic9999 <73258168+mjovanovic9999@users.noreply.github.com>
2021-12-27 04:42:33 +0100

    path finding da nije prekinut

--- Implementacija/moves.py
+++ Implementacija/moves.py
@@ -145,3 +145,67 @@ def update_heat_map(heat_map: dict[tuple[int, int], int], table_size: tuple[int,
     # heat_map[position] += 1
     # dopuniti!!!!!!!!!!!!
     return heat_map
+
+
+
+
+
+def h_calculate_raw(next_pos : tuple[int, int], dest_pos: tuple[int, int])->int:#sto manja to bolja
+    return abs(next_pos[0]-dest_pos[0])+abs(next_pos[1]-dest_pos[1])
+
+def h_calculate_optimized(dimensions:tuple[int,int],next_pos:tuple[int, int], dest_pos:tuple[int, int],heat_map:dict[tuple[int,int],int])->int:
+	half_height=(dimensions[0]-1)/2
+	real= h_calculate_raw(next_pos,dest_pos) #+heat_map[generate_next_moves]#vrv neki faktor za heat map# treba inverzna logika
+	pom=(half_height-next_pos[0] if half_height>=next_pos[0] else next_pos[0]-half_height)
+	rez=real-pom
+	return rez
+
+def find_path(dimensions:tuple[int,int],vertical_walls: list[tuple[int,int]], horizontal_walls: list[tuple[int, int]], pawn_pos : tuple[int, int], dest_pos: tuple[tuple[int, int], tuple[int, int]],heat_map:dict[tuple[int,int],int])->list[tuple[int,int]]:
+	return(find_path_to_one(dimensions,vertical_walls,horizontal_walls,pawn_pos,dest_pos[0],heat_map),find_path_to_one(dimensions,vertical_walls,horizontal_walls,pawn_pos,dest_pos[0],heat_map))
+
+def find_path_to_one(dimensions:tuple[int,int],vertical_walls: list[tuple[int,int]], horizontal_walls: list[tuple[int, int]], pawn_pos : tuple[int, int], dest_pos:tuple[int, int],heat_map:dict[tuple[int,int],int])->list[tuple[int,int]]:
+    # if start[0]<0 or start[0]>5 or end[0]<0 or end[0]>5 or start[1]<0 or start[1]>5 or end[1]<0 or end[1]>5:
+    #     return "Losi parametri"
+    # if start[0]==end[0] and start[1]==end[1]:
+    #     return []
+    found_end = False        
+    open_set = set([pawn_pos])  
+    closed_set = set() 
+    g = {}                   
+    prev_nodes = {}          
+    g[pawn_pos] = 0          
+    prev_nodes[pawn_pos] = None 
+    while len(open_set) > 0 and (not found_end): 
+        node = None 
+        for next_node in open_set: 
+            if node is None or g[next_node] + h_calculate_optimized(dimensions,next_node,dest_pos,heat_map) < g[node] + h_calculate_optimized(dimensions,next_node,dest_pos,heat_map): 
+                node = next_node
+        if node == dest_pos: 
+            found_end = True 
+            break
+        # print(node,end="")
+        for m in generate_next_moves(dimensions,vertical_walls,horizontal_walls,node,dest_pos): 
+            cost=h_calculate_optimized(dimensions,m,dest_pos,heat_map)
+            if m not in open_set and  m not in closed_set: 
+                open_set.add(m) 
+                prev_nodes[m] = node 
+                g[m] = g[node] + cost 
+            elif m not in closed_set and g[m] > g[node] + cost: 
+                g[m] = g[node] + cost 
+                prev_nodes[m] = node 
+        open_set.remove(node) 
+        closed_set.add(node)
+    path = [] 
+    if found_end: 
+        prev = dest_pos 
+        while prev_nodes[prev] is not None: 
+            path.append(prev) 
+            prev = prev_nodes[prev] 
+        path.append(pawn_pos) 
+        path.reverse()       
+    return path
+
+def generate_next_moves(dimensions:tuple[int,int],vertical_walls: list[tuple[int,int]], horizontal_walls: list[tuple[int, int]], pawn_pos : tuple[int, int],dest_pos:tuple[int, int])->list[tuple[int,int]]:
+	if abs(dest_pos[0]-pawn_pos[0])+abs(dest_pos[1]-pawn_pos[1])==1 and is_player_movement_valid(vertical_walls,horizontal_walls,dimensions,pawn_pos,dest_pos)==True:
+		return[dest_pos]
+	return list(filter(lambda x:is_player_movement_valid(vertical_walls,horizontal_walls,dimensions,pawn_pos,x),map( lambda x:(pawn_pos[0]+x[0],pawn_pos[1]+x[1]) ,[(-2,0),(-1,-1),(-1,1),(0,-2),(0,2),(1,-1),(1,1),(2,0)])))
